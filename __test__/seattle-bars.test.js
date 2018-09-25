76b4ac673628d115be63e01ec04e3a2b66bf53a6
Ashabrai Frauen <ashabraifrauen@yahoo.com>
2018-09-24 18:52:35 -0700

    added a readme and changed gitignore file

--- __test__/seattle-bars.test.js
+++ __test__/seattle-bars.test.js
@@ -4,53 +4,53 @@ const superagent = require('superagent');
 const server = require('../src/lib/server');
 
 describe('/api/seattlebars', () => {
-    beforeAll(server.start);
+  beforeAll(server.start);
 
-    test('should respond with 200 status code and a new json note', () => {
-        return superagent.post('http://localhost:3000/api/seattlebar')
-            .set('Content-Type', 'application/json')
-            .send({
-                title: 'Canon',
-                content: 'Whiskey bar',
-            })
-            .then((response) => {
-                expect(response.status).toEqual(200);
-                expect(response.body.content).toEqual('Whiskey bar');
-                expect(response.body.title).toEqual('Canon');
+  test('should respond with 200 status code and a new json note', () => {
+    return superagent.post('http://localhost:3000/api/seattlebar')
+      .set('Content-Type', 'application/json')
+      .send({
+        title: 'Canon',
+        content: 'Whiskey bar',
+      })
+      .then((response) => {
+        expect(response.status).toEqual(200);
+        expect(response.body.content).toEqual('Whiskey bar');
+        expect(response.body.title).toEqual('Canon');
 
-                expect(response.body.location).toBeTruthy();
-                expect(response.body.id).toBeTruthy();
-            });
-    });
-    test('should respond with 400 status code if there is no title', () => {
-        return superagent.post('http://localhost:3000/api/seattlebars')
-            .set('Content-Type', 'application/json')
-            .send({
-                content: 'Whiskey bar',
-            })
-            .then(Promise.reject)
-            .catch((response) => {
-                expect(response.status).toEqual(400);
-            });
-    });
+        expect(response.body.location).toBeTruthy();
+        expect(response.body.id).toBeTruthy();
+      });
+  });
+  test('should respond with 400 status code if there is no title', () => {
+    return superagent.post('http://localhost:3000/api/seattlebars')
+      .set('Content-Type', 'application/json')
+      .send({
+        content: 'Whiskey bar',
+      })
+      .then(Promise.reject)
+      .catch((response) => {
+        expect(response.status).toEqual(400);
+      });
+  });
 
-    test('should respond 200, verifying the route is valid', () => {
-        return superagent.get('http://localhost:3000/api/seattlebars')
-            .then((response) => {
-                expect(response.status).toEqual(200);
-            });
+  test('should respond 200, verifying the route is valid', () => {
+    return superagent.get('http://localhost:3000/api/seattlebars')
+      .then((response) => {
+        expect(response.status).toEqual(200);
+      });
+  });
+  test('should respond with 200 if path and query are both valid', () => {
+    return superagent.get('http://localhost:3000/api/seattlebars?title=Canon')
+      .then((response) => {
+        expect(response.status).toEqual(200);
+      });
+  });
+});
+test('should respond with 200, and removed json seattle bar', () => {
+  return superagent.delete('http://localhost:3000/api/seattlebars?title=Cannon')
+    .set('Content-Type', 'application/json')
+    .then((response) => {
+      expect(response.status).toEqual(204);
     });
-    test('should respond with 200 if path and query are both valid', () => {
-        return superagent.get('http://localhost:3000/api/seattlebars?title=Canon')
-            .then((response) => {
-                expect(response.status).toEqual(200);
-            })
-        });
-    });
-    test('should respond with 200, and removed json seattle bar', () => {
-        return superagent.delete('http://localhost:3000/api/seattlebars?title=Cannon')
-            .set('Content-Type', 'application/json')
-            .then((response) => {
-                expect(response.status).toEqual(204);
-            });
-    });
+});
--- src/lib/server.js
+++ src/lib/server.js
@@ -16,9 +16,9 @@ const server = module.exports = {};
  * @returns the result of app.listen
  */
 server.start = (port = 3000) => {
-    return app.listen(port, () => {
-        logger.log(logger.INFO, `Server is on at PORT: ${port}`);
-    });
+  return app.listen(port, () => {
+    logger.log(logger.INFO, `Server is on at PORT: ${port}`);
+  });
 };
 
 // server.stop = (callback) => {
